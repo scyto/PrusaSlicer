1a5887132fdcd7b7574044b1a6459c584279e7c9
tamasmeszaros <meszaros.q@gmail.com>
2022-01-07 10:15:16 +0100

    SLA archiver implemented for svg output, switchable in config.
    new config is sla_archive_format as a string.
    WIP
    
    
    Get rid of SVG class, use manual svg creation
    
    
    Revert changes in SVG.hpp and SVG.cpp

--- src/libslic3r/PrintConfig.cpp
+++ src/libslic3r/PrintConfig.cpp
@@ -3776,6 +3776,11 @@ void PrintConfigDef::init_sla_params()
     def->enum_labels.push_back(L("Fast"));
     def->mode = comAdvanced;
     def->set_default_value(new ConfigOptionEnum<SLAMaterialSpeed>(slamsFast));
+
+    def = this->add("sla_archive_format", coString);
+    def->label = L("Format of the output SLA archive");
+    def->mode = comAdvanced;
+    def->set_default_value(new ConfigOptionString("SL1"));
 }
 
 void PrintConfigDef::handle_legacy(t_config_option_key &opt_key, std::string &value)
